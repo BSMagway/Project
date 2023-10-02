6f5ae75e76181b89c1d4a7565fe1cb471357935d
BSMagway <bsmagway@gmail.com>
2023-10-02 23:17:25 +0300

    Jwt

--- Project/ViewModels/PartialMainWindow/Services.cs
+++ Project/ViewModels/PartialMainWindow/Services.cs
@@ -18,6 +18,9 @@ namespace Project.ViewModels
         /// </summary>
         IWorkWithBDGeneric<MoistureSoilTest> _moistureSoilTestDBService;
 
+        /// <summary>
+        /// Сервис для работы с базой данных всех тестов с общей информацией о них.
+        /// </summary>
         IWorkWithBDGeneric<Test> _testDBService;
 
 
