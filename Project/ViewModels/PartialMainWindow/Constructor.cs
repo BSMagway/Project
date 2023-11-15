9e63881dc10beac5bb47894e41698f2a3de34b50
BSMagway <bs_magway@tut.by>
2023-11-15 17:22:30 +0300

    Команды для тестов в конструктор.

--- Project/ViewModels/PartialMainWindow/Constructor.cs
+++ Project/ViewModels/PartialMainWindow/Constructor.cs
@@ -70,16 +70,55 @@ namespace Project.ViewModels
             SelectTypeTestCommand = new LambdaCommand(OnSelectTypeTestCommandExecuted, CanSelectTypeTestCommandExecute);
             ReturnToNewTaskPageCommand = new LambdaCommand(OnReturnToNewTaskPageCommandExecuted, CanReturnToNewTaskPageCommandExecute);
             SelectSoilTestCommand = new LambdaCommand(OnSelectSoilTestCommandExecuted, CanSelectSoilTestCommandExecute);
+            
             CalculateMoistureSoilTestCommand = new LambdaCommand(OnCalculateMoistureSoilTestCommandExecuted, CanCalculateMoistureSoilTestCommandExecute);
             SaveMoistureSoilTestCommand = new LambdaCommand(OnSaveMoistureSoilTestCommandExecuted, CanSaveMoistureSoilTestCommandExecute);
             CalculateRollingBoundarySoilTestCommand = new LambdaCommand(OnCalculateRollingBoundarySoilTestCommandExecuted, CanCalculateRollingBoundarySoilTestCommandExecute);
             SaveRollingBoundarySoilTestCommand = new LambdaCommand(OnSaveRollingBoundarySoilTestCommandExecuted, CanSaveRollingBoundarySoilTestCommandExecute);
+            CalculateYieldLimitSoilTestCommand = new LambdaCommand(OnCalculateYieldLimitSoilTestCommandExecuted, CanCalculateYieldLimitSoilTestCommandExecute);
+            SaveYieldLimitSoilTestCommand = new LambdaCommand(OnSaveYieldLimitSoilTestCommandExecuted, CanSaveYieldLimitSoilTestCommandExecute);
+            CalculateDensitySoilTestCommand = new LambdaCommand(OnCalculateDensitySoilTestCommandExecuted, CanCalculateDensitySoilTestCommandExecute);
+            SaveDensitySoilTestCommand = new LambdaCommand(OnSaveDensitySoilTestCommandExecuted, CanSaveDensitySoilTestCommandExecute);
+
+            CalculateWeakGrainsGravelTestCommand = new LambdaCommand(OnCalculateWeakGrainsGravelTestCommandExecuted, CanCalculateWeakGrainsGravelTestCommandExecute);
+            SaveWeakGrainsGravelTestCommand = new LambdaCommand(OnSaveWeakGrainsGravelTestCommandExecuted, CanSaveWeakGrainsGravelTestCommandExecute);
+            CalculateMoistureGravelTestCommand = new LambdaCommand(OnCalculateMoistureGravelTestCommandExecuted, CanCalculateMoistureGravelTestCommandExecute);
+            SaveMoistureGravelTestCommand = new LambdaCommand(OnSaveMoistureGravelTestCommandExecuted, CanSaveMoistureGravelTestCommandExecute);
+            CalculateFlakyGrainsGravelTestCommand = new LambdaCommand(OnCalculateFlakyGrainsGravelTestCommandExecuted, CanCalculateFlakyGrainsGravelTestCommandExecute);
+            SaveFlakyGrainsGravelTestCommand = new LambdaCommand(OnSaveFlakyGrainsGravelTestCommandExecuted, CanSaveFlakyGrainsGravelTestCommandExecute);
+            CalculateDustGravelTestCommand = new LambdaCommand(OnCalculateDustGravelTestCommandExecuted, CanCalculateDustGravelTestCommandExecute);
+            SaveDustGravelTestCommand = new LambdaCommand(OnSaveDustGravelTestCommandExecuted, CanSaveDustGravelTestCommandExecute);
+            CalculateCrushedGrainsGravelTestCommand = new LambdaCommand(OnCalculateCrushedGrainsGravelTestCommandExecuted, CanCalculateCrushedGrainsGravelTestCommandExecute);
+            SaveCrushedGrainsGravelTestCommand = new LambdaCommand(OnSaveCrushedGrainsGravelTestCommandExecuted, CanSaveCrushedGrainsGravelTestCommandExecute);
+            CalculateCrushabilityGravelTestCommand = new LambdaCommand(OnCalculateCrushabilityGravelTestCommandExecuted, CanCalculateCrushabilityGravelTestCommandExecute);
+            SaveCrushabilityGravelTestCommand = new LambdaCommand(OnSaveCrushabilityGravelTestCommandExecuted, CanSaveCrushabilityGravelTestCommandExecute);
+            CalculateClayInLumpsGravelTestCommand = new LambdaCommand(OnCalculateClayInLumpsGravelTestCommandExecuted, CanCalculateClayInLumpsGravelTestCommandExecute);
+            SaveClayInLumpsGravelTestCommand = new LambdaCommand(OnSaveClayInLumpsGravelTestCommandExecuted, CanSaveClayInLumpsGravelTestCommandExecute);
+            CalculateBulkDensityGravelTestCommand = new LambdaCommand(OnCalculateBulkDensityGravelTestCommandExecuted, CanCalculateBulkDensityGravelTestCommandExecute);
+            SaveBulkDensityGravelTestCommand = new LambdaCommand(OnSaveBulkDensityGravelTestCommandExecuted, CanSaveBulkDensityGravelTestCommandExecute);
+
+            CalculateBulkDensitySandAndGravelTestCommand = new LambdaCommand(OnCalculateBulkDensitySandAndGravelTestCommandExecuted, CanCalculateBulkDensitySandAndGravelTestCommandExecute);
+            SaveBulkDensitySandAndGravelTestCommand = new LambdaCommand(OnSaveBulkDensitySandAndGravelTestCommandExecuted, CanSaveBulkDensitySandAndGravelTestCommandExecute);
+
+            CalculateFiltrationPlaneGeotextileTestCommand = new LambdaCommand(OnCalculateFiltrationPlaneGeotextileTestCommandExecuted, CanCalculateFiltrationPlaneGeotextileTestCommandExecute);
+            SaveFiltrationPlaneGeotextileTestCommand = new LambdaCommand(OnSaveFiltrationPlaneGeotextileTestCommandExecuted, CanSaveFiltrationPlaneGeotextileTestCommandExecute);
+
+            CalculateMoistureSandTestCommand = new LambdaCommand(OnCalculateMoistureSandTestCommandExecuted, CanCalculateMoistureSandTestCommandExecute);
+            SaveMoistureSandTestCommand = new LambdaCommand(OnSaveMoistureSandTestCommandExecuted, CanSaveMoistureSandTestCommandExecute);
+            CalculateDustSandTestCommand = new LambdaCommand(OnCalculateDustSandTestCommandExecuted, CanCalculateDustSandTestCommandExecute);
+            SaveDustSandTestCommand = new LambdaCommand(OnSaveDustSandTestCommandExecuted, CanSaveDustSandTestCommandExecute);
+            CalculateBulkDensitySandTestCommand = new LambdaCommand(OnCalculateBulkDensitySandTestCommandExecuted, CanCalculateBulkDensitySandTestCommandExecute);
+            SaveBulkDensitySandTestCommand = new LambdaCommand(OnSaveBulkDensitySandTestCommandExecuted, CanSaveBulkDensitySandTestCommandExecute);
+
+
             OpenSelectCustomerCommand = new LambdaCommand(OnOpenSelectCustomerCommandExecuted, CanOpenSelectCustomerCommandExecute);
             LoadCustomerFromListCommand = new LambdaCommand(OnLoadCustomerFromListCommandExecuted, CanLoadCustomerFromListCommandExecute);
             LoadTestFromBDCommand = new LambdaCommand(OnLoadTestFromBDCommandExecuted, CanLoadTestFromBDCommandExecute);
+ 
             OpenFormForCustomerAddCommand = new LambdaCommand(OnOpenFormForCustomerAddCommandExecuted, CanOpenFormForCustomerAddCommandExecute);
             OpenFormForCustomerEditCommand = new LambdaCommand(OnOpenFormForCustomerEditCommandExecuted, CanOpenFormForCustomerEditCommandExecute);
             SaveEditCustomerInBDCommand = new LambdaCommand(OnSaveEditCustomerInBDCommandExecuted, CanSaveEditCustomerInBDCommandExecute);
+
             LoginUserCommand = new LambdaCommand(OnLoginUserCommandExecuted, CanLoginUserCommandExecute);
             OpenRegisterUserFormCommand = new LambdaCommand(OnOpenRegisterUserFormCommandExecuted, CanOpenRegisterUserFormCommandExecute);
             ReturnToLoginUserFormCommand = new LambdaCommand(OnReturnToLoginUserFormCommandExecuted, CanReturnToLoginUserFormCommandExecute);
